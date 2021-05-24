149841850843d887610ade00118db68398a7b53c
Yan <yan71884@gmail.com>
2021-05-24 14:55:30 +0300

    Add: Task 1 in progress

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,13 +5,51 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    NSString *a = [[NSString alloc] init];
+    switch (monthNumber) {
+        case 1: a = @"January";
+            break;
+        case 2: a = @"February";
+            break;
+        case 3: a = @"March";
+            break;
+        case 4: a = @"April";
+            break;
+        case 5: a = @"May";
+            break;
+        case 6: a = @"June";
+            break;
+        case 7: a = @"July";
+            break;
+        case 8: a = @"August";
+            break;
+        case 9: a = @"September";
+            break;
+        case 10: a = @"October";
+            break;
+        case 11: a = @"November";
+            break;
+        case 12: a = @"December";
+            break;
+        default: a = nil;
+            break;
+    }
+    return a;
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    long d;
+    NSArray *list = [date componentsSeparatedByString:@"-"];
+    if ([[list objectAtIndex:1] intValue] >= 01 || [[list objectAtIndex:1] intValue] <= 12) {
+        NSString *b = [list objectAtIndex:2];
+        NSArray *list2 = [b componentsSeparatedByString:@"T"];
+        NSString *c = [list2 objectAtIndex:0];
+        return d = [c longLongValue];
+    } else {
+        return d = 0;
+    }
 }
 
 #pragma mark - Third
